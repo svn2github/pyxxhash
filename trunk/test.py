c7a9ff05768a64fb17a59044781eea2ba17827e0
monkgau@gmail.com <monkgau@gmail.com@2a6d6cba-cf18-a1d6-d553-42a4ae9d698f>
2013-01-02 08:56:38 +0000

    update demo as xxhash&pyxxhash updated
    git-svn-id: http://pyxxhash.googlecode.com/svn@12 2a6d6cba-cf18-a1d6-d553-42a4ae9d698f

--- trunk/test.py
+++ trunk/test.py
@@ -5,5 +5,4 @@ char ="a"
 n=19
 for i in range(n):
     data=char *(i+1)
-    print " %-20s %-s 0 %-10d " %(data,' ',pyxxhash.fast32(data,len(data),0))
-
+    print " %-20s %-s 0 %-10d " %(data,' ',pyxxhash.xxh32(data,len(data),0))
